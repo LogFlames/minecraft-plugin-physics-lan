ed7ee42a3e1a2f971361848952daa118e3b90a5d
William Kraft <william.z.kraft@gmail.com>
2022-10-12 16:31:07 +0200

    Add java repo

---
+++ src/main/java/minecraft_mod/App.java
@@ -0,0 +1,13 @@
+package minecraft_mod;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
